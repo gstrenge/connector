7ed03616363edea6340b1776975ed71589d16015
Sebastian Arboleda <sebasarboleda22@gmail.com>
2018-06-14 19:43:36 -0400

    Added functionalities inside function and restructured as a python module

--- mqtt_connector/subscriber.py
+++ mqtt_connector/subscriber.py
@@ -6,33 +6,37 @@ from datetime import datetime
 host = "35.237.36.219"
 port = 1883
 keepalive = 30
-client1_id = "ENC_Connector"
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code {}".format(rc))
-# Subscribing in on_connect() means that if we lose the connection and
-# reconnect then subscriptions will be renewed.
-    client.subscribe("RasPi1/10Hz", 2)
+def ten_hz_client():
+	"""
+	MQTT Client connector in charge of receiving the 10 Hz csv files,
+	perform calculations and store them in the database
+	"""
+	client1_id = "ENC_Connector"
+	# The callback for when the client receives a CONNACK response from the server.
+	def on_connect(client1, userdata, flags, rc):
+	    print("Connected with result code {}".format(rc))
+	# Subscribing in on_connect() means that if we lose the connection and reconnect then 
+	# subscriptions will be renewed.
+	    client1.subscribe("RasPi1/10Hz", 2)
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print("received message")
-    with open('random.csv', 'wb') as fd:
-        fd.write(msg.payload)
+	# The callback for when a PUBLISH message is received from the server.
+	def on_message(client1, userdata, msg):
+	    print("received message")
+	    with open('random.csv', 'wb') as fd:
+	        fd.write(msg.payload)
 
-if __name__ == '__main__':
-	# Client constructor optional params ->
-	# Client(client_id=””, clean_session=True, userdata=None, protocol=MQTTv311, transport=”tcp”)
+	# Params -> Client(client_id=””, clean_session=True, userdata=None, protocol=MQTTv311, transport=”tcp”)
 	# We set clean_session False, so in case connection is lost, it'll reconnect with same ID
 	client1 = mqtt.Client(client_id=client1_id, clean_session=False)
 	client1.on_connect = on_connect
 	client1.on_message = on_message
 
-	client.connect(host, port, keepalive)
+	client1.connect(host, port, keepalive)
 
 	# Blocking call that processes network traffic, dispatches callbacks and
 	# handles reconnecting.
-	# Other loop*() functions are available that give a threaded interface and a
-	# manual interface.
-	client.loop_forever()
+	client1.loop_forever()
+
+if __name__ == '__main__':
+	ten_hz_client()
--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,3 +1,7 @@
 import unittest
 
 from mqtt_connector import subscriber
+
+def test_connectivity():
+	from subscriber import ten_hz_client
+	
