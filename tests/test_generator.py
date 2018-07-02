485ff679386463ffb3426404501ba2ad0471205a
Sebastian Arboleda <sebasarboleda22@gmail.com>
2018-07-02 13:21:54 -0400

    Deleted broker connection test

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -6,14 +6,14 @@ from datetime import datetime
 from influxdb import DataFrameClient
 
 def test_write_to_db():
-	from app.subscriber import connect_to_db, write_to_db
+	from app.app import write_to_db
 	db_host = "localhost"
 	db_port = 8086
 	db_username = "root"
 	db_password = "root"
 	db_database = "test"
 	#Connects to local InfluxDB
-	db_client = connect_to_db(host=db_host, port=db_port, username=db_username, 
+	db_client = DataFrameClient(host=db_host, port=db_port, username=db_username, 
 		password=db_password, database=db_database)
 	#Creates local Database
 	db_client.create_database('test')
@@ -36,15 +36,3 @@ def test_write_to_db():
 	assert value == 100*0.125
 	#bug : dataframe.index.values[0] has more precision than np.datetime64(now)
 	#assert dataframe.index.values[0] == np.datetime64(now)
-
-def test_broker_connection():
-	from app.subscriber import connect_to_broker, subscribe_to_topic
-	host = "35.237.36.219"
-	port = 1883
-	client_id = "TESTING_CLIENT"
-	topic = "testing/topic"
-	keepalive = 30
-	def on_connect(client, userdata, flags, rc):
-		subscribe_to_topic(topic=topic, client=client)
-	client, connection = connect_to_broker(client_id=client_id, host=host, port=port, on_connect=on_connect, on_message=None, keepalive=keepalive)
-	assert connection == 0
