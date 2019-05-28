6299b707b4f85bdf0ccef905b4a5d21e08937ce9
Seb <sebasarboleda22@gmail.com>
2019-05-28 18:49:28 -0400

    testing

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker-compose -f docker-compose.test.yml up connector_test
+docker-compose -f docker-compose.test.yml up
 
 # Destroy containers
 docker-compose -f docker-compose.test.yml down
